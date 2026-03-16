5d2dcb340ad82035646fb18205b4dbd42b73614b
keyuv03 <joshbeltrame@outlook.com>
2026-03-15 21:53:57 -0400

    Adding some new files

---
+++ center_of_gravity.py
@@ -0,0 +1,29 @@
+'''
+
+Calculates and returns the X ordinate of the center of gravity of the aircraft
+
+'''
+
+class CG:
+
+    def __init__(self):
+
+        self.XCG = 0
+        self.MTOW = 0
+        self.moment = 0
+
+
+    def add_elem(self, W, X):
+        
+        self.MTOW += W
+        self.moment = W*X
+    
+
+    def calc_XCG(self):
+
+        self.XCG = (self.moment/(self.MTOW))
+    
+
+    def get_XCG(self):
+
+        return self.XCG
